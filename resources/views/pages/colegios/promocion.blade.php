aaf69a99e90da8ff2c766aae7b2a8284ee10110f
Pool Valles <programador3.forma@gmail.com>
2024-04-30 16:23:26 -0500

    fix: by

--- resources/views/pages/colegios/promocion.blade.php
+++ resources/views/pages/colegios/promocion.blade.php
@@ -3,22 +3,27 @@
     $title = 'Promoción';
 
     /*HERO SECTION*/
-    $color = 'light'; // 'dark' || 'light'
+    $color = 'dark'; // 'dark' || 'light'
     $text_move = ' Promoción · Promoción ·';
     $img_hero = '/assets/video-corporativo/conferencias.webp';
-    $description = '';
+    $description = 'Explora el mundo de las credenciales y descubre cómo estas 
+                    pequeñas tarjetas pueden abrir grandes puertas para tu identidad y seguridad.';
     /*END HERO SECTION*/
 
     /*CONTENT SECTION*/
     $content_color = 'light'; // 'dark' || 'light'
-    $content_data_bgcolor = $content_color == 'dark' ? '#fff' : '#000'; // 'dark' || 'light'
+    $content_data_bgcolor = $content_color == 'light' ? '#fff' : '#000'; // 'dark' || 'light'
     /*CONTENT SECTION*/
 @endphp
 
 @extends('layouts.articulo', ['title' => $title, 'img_hero' => $img_hero, 'text_move' => $text_move, 'description' => $description, 'color' => $color])
 
 @section('content')
-    
+    <!-- Row -->
+    <div class="content-row row_padding_top row_padding_bottom {{ $content_color }}-section change-header-color"
+        data-bgcolor="{{ $content_data_bgcolor }}">
+
+
         <!-- Main Content -->
         <div id="main-content">
             <!-- Main Page Content -->
@@ -33,99 +38,234 @@
 
                     <hr>
 
-                    <div class="gallery">
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/1.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/2.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/3.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/4.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/5.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/6.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/7.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/8.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/9.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/10.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/11.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/12.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/13.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/14.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/15.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/16.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/17.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/18.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/19.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/20.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/21.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/22.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/23.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/24.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/25.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/26.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/27.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/28.webp" alt="Image Title"></a>
-                        </div>
-                        <div class="item">
-                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/29.webp" alt="Image Title"></a>
-                        </div>
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/1.webp"
+                                alt="Image Title"></a>
+                    </figure>
 
-                        <!-- Repite este bloque para todas las imágenes -->
+                    <hr>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="video-link"><img src="/assets/colegios/promocion/2.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
                     </div>
 
-                    
+                    <div class="one_half ">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/3.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/4.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/5.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="video-link"><img src="/assets/colegios/promocion/6.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <div class="one_half ">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/7.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/8.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/9.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="video-link"><img src="/assets/colegios/promocion/10.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <div class="one_half ">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/11.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/12.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/13.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="video-link"><img src="/assets/colegios/promocion/14.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/15.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/16.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <figure class=" has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/17.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="video-link"><img src="/assets/colegios/promocion/18.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/19.webp"
+                                    alt="Image Title"></a>
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class="has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/20.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="video-link"><img src="/assets/colegios/promocion/21.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/22.webp"
+                                    alt="Image Title"></a>
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class="has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/23.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="video-link"><img src="/assets/colegios/promocion/24.webp"
+                                    alt="Image Title"></a>
+
+                        </figure>
+                    </div>
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/25.webp"
+                                    alt="Image Title"></a>
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class="has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/26.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr> 
+
+                    <div class="one_half">
+                        <figure class=" has-cover">
+                            <a href="#" class="image-link"><img src="/assets/colegios/promocion/27.webp"
+                                    alt="Image Title"></a>
+                        </figure>
+                    </div>
+
+                    <hr>
+
+                    <figure class="has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/28.webp"
+                                alt="Image Title"></a>
+                    </figure>
+
+                    <hr>
+
+                    <figure class="has-cover">
+                        <a href="#" class="image-link"><img src="/assets/colegios/promocion/29.webp"
+                                alt="Image Title"></a>
+                    </figure>
+                </div>
                 <!--/Row -->
             @endsection
 
